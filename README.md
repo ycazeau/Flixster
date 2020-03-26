9f27a4f104306bc031179e3ecd8593abd2a7fa48
ycazeau <ycazeau23@gmail.com>
2020-03-26 14:54:41 -0400

    Details Screen implementation

---
+++ README.md
@@ -0,0 +1,32 @@
+# Flixster App Part 1
+Flixster is an application that allows users to browse movies from the [The Movie Database API](http://docs.themoviedb.apiary.io/#)
+
+### User Stories
+
+#### REQUIRED (10pts)
+- [X] (10pts) User can view a list of movies (title, poster image, and overview) currently playing in theaters from the Movie Database API.
+
+#### BONUS
+- [X] (2pts) Views should be responsive for both landscape/portrait mode.
+   - [X] (1pt) In portrait mode, the poster image, title, and movie overview is shown.
+   - [X] (1pt) In landscape mode, the rotated alternate layout should use the backdrop image instead and show the title and movie overview to the right of it.
+
+- [ ] (2pts) Display a nice default [placeholder graphic](https://guides.codepath.org/android/Displaying-Images-with-the-Glide-Library#advanced-usage) for each image during loading
+- [X] (2pts) Improved the user interface by experimenting with styling and coloring.
+- [ ] (2pts) For popular movies (i.e. a movie voted for more than 5 stars), the full backdrop image is displayed. Otherwise, a poster image, the movie title, and overview is listed. Use Heterogenous RecyclerViews and use different ViewHolder layout files for popular movies and less popular ones.
+
+### App Walkthough GIF
+
+### App Walkthough GIF Portrait Mode
+<img src="flixsterPortrait.gif" width=250><br>
+
+### App Walkthough GIF Landscape Mode
+<img src="flixsterLandscape.gif" width=250><br>
+
+### Notes
+The challenge was to display the default place holder...
+
+### Open-source libraries used
+
+- [Android Async HTTP](https://github.com/codepath/CPAsyncHttpClient) - Simple asynchronous HTTP requests with JSON parsing
+- [Glide](https://github.com/bumptech/glide) - Image loading and caching library for Androids
---
+++ app/src/main/java/adapters/MovieAdapter.java
@@ -0,0 +1,105 @@
+package adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.flixster.DetailActivity;
+import com.example.flixster.R;
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
+import models.Movie;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
+
+    Context context;
+    List<Movie> movies;
+
+    public MovieAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    // Usually involves inflating a layout from XML and returning the Holder
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("MovieAdapter","onCreateViewHolder");
+        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+        return new ViewHolder(movieView);
+    }
+
+    // Involves populating data into the item through Holder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("MovieAdapter","onBindViewHolder" + position);
+        // Get the movie at the passed in position
+        Movie movie = movies.get(position);
+        // Bind the movie data into the View Holder
+        holder.bind(movie);
+
+    }
+
+    // Returns the total count of items in the list
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        RelativeLayout container;
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
+        }
+
+        public void bind(final Movie movie) {
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+            String  imageUrl;
+            // if phone is an landscape
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                // the imageUrl = back drop image
+                imageUrl = movie.getBackdropPath();
+            } else {
+                // else imageUrl = poster image
+                imageUrl = movie.getPosterPath();
+            }
+            Glide.with(context).load(imageUrl).into(ivPoster);
+
+            // 1. Register click listener on the whole row
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // 2. Navigate to a new Activity
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie" , Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
+        }
+    }
+}

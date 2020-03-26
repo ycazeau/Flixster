20c7324a0c0a0fecf4f9dd1f07cfc02bb4a9133d
ycazeau <ycazeau23@gmail.com>
2020-03-26 17:14:15 -0400

    Adding Youtube Player API

--- app/src/main/java/com/example/flixster/DetailActivity.java
+++ app/src/main/java/com/example/flixster/DetailActivity.java
@@ -3,18 +3,28 @@ package com.example.flixster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.RatingBar;
 import android.widget.TextView;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 import org.parceler.Parcels;
 
 import models.Movie;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends YouTubeBaseActivity {
+
+    private static final String YOUTUBE_API_KEY= "AIzaSyBiThCS9V6T2vFeG62p0kc2NZ4m9VqLq-o";
 
     TextView tvTitle;
     TextView tvOverview;
     RatingBar ratingBar;
+
+    YouTubePlayerView youTubePlayerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +35,26 @@ public class DetailActivity extends AppCompatActivity {
         tvOverview = findViewById(R.id.tvOverview);
         ratingBar = findViewById(R.id.ratingBar);
 
+        youTubePlayerView = findViewById(R.id.player);
+
 
         Movie movie = Parcels.unwrap(getIntent().getParcelableExtra("movie"));
         tvTitle.setText(movie.getTitle());
         tvOverview.setText(movie.getOverview());
         ratingBar.setRating((float) movie.getRating());
+
+        youTubePlayerView.initialize(YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                Log.d("DetailActivity", "onInitializationSuccess");
+
+                youTubePlayer.cueVideo("5xVh-7ywKpE");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.d("DetailActivity", "onInitializationFailure");
+            }
+        });
     }
 }
